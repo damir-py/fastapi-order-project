2664455d989de0955817f3f3af01939be0084641
Damir <nimadjonov.2404@gmail.com>
2025-01-30 15:57:04 +0500

    done update and delete products

--- app/product_routers.py
+++ app/product_routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.models import Response
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.exceptions import HTTPException
 
@@ -7,7 +8,6 @@ from app.auth_utils import decode_access_token
 from app.database import Session, get_db
 
 from app.models import User, Product
-from app.order_routers import order_router
 from app.schemas import ProductModel
 
 product_router = APIRouter(
@@ -17,7 +17,7 @@ product_router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
-@product_router.post('/create/')
+@product_router.post('/create/', status_code=201)
 async def create_product(product: ProductModel, token: oauth2_scheme = Depends(), db: Session = Depends(get_db)):
     payload = decode_access_token(token)
     if payload is None:
@@ -50,7 +50,7 @@ async def create_product(product: ProductModel, token: oauth2_scheme = Depends()
     return res
 
 
-@product_router.get('/list/')
+@product_router.get('/list/', status_code=200)
 async def get_product_list(token: oauth2_scheme = Depends(), db: Session = Depends(get_db)):
     payload = decode_access_token(token)
     if payload is None:
@@ -73,7 +73,7 @@ async def get_product_list(token: oauth2_scheme = Depends(), db: Session = Depen
     raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="U dont have access!")
 
 
-@product_router.get("/{pk}/")
+@product_router.get("/{pk}/", status_code=200)
 async def get_product_by_id(pk: int, token: oauth2_scheme = Depends(), db: Session = Depends(get_db)):
     payload = decode_access_token(token)
     if payload is None:
@@ -93,6 +93,70 @@ async def get_product_by_id(pk: int, token: oauth2_scheme = Depends(), db: Sessi
         "id": product.id,
         "name": product.name,
         "price": product.price
+    }
+
+    return res
+
+
+@product_router.delete('/{pk}/', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product_by_id(pk: int, token: oauth2_scheme = Depends(), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token!")
+
+    user = db.query(User).filter(User.id == payload.get('user_id')).first()
+
+    if user.is_staff is False:
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="U don't have right access!")
+
+    product = db.query(Product).filter(Product.id == pk).first()
+
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Order with id: < {pk} > not fount!")
+
+    db.delete(product)
+    db.commit()
+
+    res = {
+        "message": "Successfully deleted!",
+        "ok": True
+    }
+
+    return res
+
+
+@product_router.put("/update/{pk}/", status_code=status.HTTP_200_OK)
+async def update_product(pk: int, data: ProductModel, token: oauth2_scheme = Depends(), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token!")
+
+    user = db.query(User).filter(User.id == payload.get('user_id')).first()
+
+    if user.is_staff is False:
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="U don't have right access!")
+
+    product = db.query(Product).filter(Product.id == pk).first()
+
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Order with id: < {pk} > not fount!")
+
+    # update product
+
+    for key, val in data.dict(exclude_unset=True).items():
+        setattr(product, key, val)
+
+    db.commit()
+
+    res = {
+        "ok": True,
+        "code": 200,
+        "message": f"Product with ID {pk} successfully updated!",
+        "data": {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
     }
 
     return res
